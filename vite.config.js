d6fa2ad1d0c3011b1568d672446cfb2a2523cd4a
RamKumar-cmd-dot <ram.kumar@students.iiit.ac.in>
2026-02-15 00:27:12 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
   plugins: [svelte()],
+  base: '/0x1337-website/',  // Replace with YOUR repo name
   server: {
     port: 5173
   }
